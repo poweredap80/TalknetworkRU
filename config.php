ebf2a04d2728b799c9429ec2d870ecac0b410c46
poweredap80 <68704433+poweredap80@users.noreply.github.com>
2020-07-23 20:30:31 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.2.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\mysqli';
+$dbhost = 'localhost';
+$dbport = '';
+$dbname = 'u0407395_phpb324';
+$dbuser = 'u0407395_phpb324';
+$dbpasswd = '3.B-4pS93y';
+$table_prefix = 'phpbbgn_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
